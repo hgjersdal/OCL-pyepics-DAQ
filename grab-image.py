93c1f89a6a1b7bc77e953e4ccad832dc5be3b31f
hgjersdal <hgjersdal@gmail.com>
2017-11-02 15:41:34 +0100

    Supress warnings about identical process variables

--- grab-image.py
+++ grab-image.py
@@ -5,6 +5,15 @@ os.environ["PYEPICS_LIBCA"] = "/home/dev/git/ng3e/root/R3.15.4/base/lib/linux-x8
 import epics
 import time
 import numpy
+import warnings 
+
+# Supress CA warnings about identical variable names
+supressEpicsWarings = True
+if(supressEpicsWarings):
+    def handleMessages(text):
+        if(text.find("Identical process variable names on multiple servers") > 0):
+            sys,stderr.write(text)
+    epics.ca.replace_printf_handler(handleMessages)
 
 pvConfig = {
     'CAM1:image1:EnableCallbacks': 1,#Enable
@@ -13,6 +22,7 @@ pvConfig = {
     'CAM1:det1:DataType': 1,  #UInt16, 12-bit
     'CAM1:det1:typo': 1  #typo
 }
+
 def caputDict(dict):
     for key, value in dict.iteritems():
         epics.caput(key, value)
@@ -23,7 +33,7 @@ def caputAndCheckDict(dict):
     for key, value in dict.iteritems():
         rbv = epics.caget(key + '_RBV')
         if( rbv != value):
-            print( key + " is set to " + str(value) + ", but RBV is " + str(rbv))
+            warnings.warn( key + " is set to " + str(value) + ", but RBV is " + str(rbv))
 
 def setExposure(exposure, gain):
     caputAndCheckDict({'CAM1:det1:AcquireTime': exposure, 'CAM1:det1:Gain': gain})
@@ -38,8 +48,6 @@ def acquireImage():
 caputAndCheckDict(pvConfig)
 setExposure(0.01, 0)
 raw = acquireImage()
-print(raw)
-
 image = raw.reshape(epics.caget('CAM1:det1:SizeY_RBV'), epics.caget('CAM1:det1:SizeX_RBV'))
 import matplotlib.pyplot as plt
 plt.matshow(image)
