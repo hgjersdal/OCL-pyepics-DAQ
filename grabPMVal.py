ef02846f4c3d868f90f5095e0f4e802a8cc284e6
hgjersdal <hgjersdal@gmail.com>
2017-11-06 16:57:52 +0100

    Set wavelength for PM measurements

--- grabPMVal.py
+++ grabPMVal.py
@@ -1,5 +1,13 @@
 import grabData, time, epics
 
+def setWavelength(wavelengthInNm):
+    epics.caput('PM100:SENS:CORR:WAV', wavelengthInNm)
+    time.sleep(0.1)
+    epics.caput('PM100:SENS:CORR:WAV_RBV.PROC', 1)
+    time.sleep(0.1)
+    if(wavelengthInNm != epics.caget('PM100:SENS:CORR:WAV_RBV') ):
+        warnings.warn('PM is set to ' + str(wavelengthInNm) + ', but read back as ' + str(epics.caget('PM100:SENS:CORR:WAV_RBV')))
+
 def getPMVals(nValues, sleepTime):
     print('This will take >' + str(nValues * sleepTime) + ' seconds' )
     PMVals = []
@@ -15,5 +23,5 @@ def printPMVals(nValues, sleepTime):
     plt.plot(PMVals)
     plt.show()
 
-
+setWavelength(500)
 printPMVals(100, 0.1)
