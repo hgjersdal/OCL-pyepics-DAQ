a1e9ffaf12647d26fef392f2b81a8a0162448875
hgjersdal <hgjersdal@gmail.com>
2017-11-09 18:41:10 +0100

    Standa stage added

---
+++ moveStage.py
@@ -0,0 +1,57 @@
+from ctypes import *
+import time
+import os
+import sys
+import tempfile
+import re
+if sys.version_info >= (3,0):
+    import urllib.parse
+try:
+    from pyximc import *
+except ImportError as err:    
+    print ("Can't import pyximc module. The most probable reason is that you haven't copied pyximc.py to the working directory. See developers' documentation for details.")
+    exit()
+except OSError as err:
+    print ("Can't load libximc library. Please add all shared libraries to the appropriate places (next to pyximc.py on Windows). It is decribed in detail in developers' documentation. On Linux make sure you installed libximc-dev package.")
+    exit()
+
+open_name = 'xi-com:///dev/ximc/00001A4C'
+open_name = open_name.encode()
+
+def home():
+    device_id = lib.open_device(open_name)
+    result = lib.command_left(device_id)
+    result = lib.command_wait_for_stop(device_id, 10)
+    glob_position = 0
+    
+def get_position():
+    device_id = lib.open_device(open_name)
+    x_pos = get_position_t()
+    result = lib.get_position(device_id, byref(x_pos))
+    if result != Result.Ok:
+        print("Result: " + repr(result))
+    lib.close_device(byref(cast(device_id, POINTER(c_int))))
+    return(x_pos.Position)
+
+def move_to (position):
+    current_pos = get_position()
+    time.sleep(1)
+    device_id = lib.open_device(open_name)
+    distance = position - current_pos
+    print("Moving from " + str(current_pos) + ' to ' + str(position))
+    result = lib.command_movr(device_id, distance, 0)
+    result = lib.command_wait_for_stop(device_id, 10)
+    lib.close_device(byref(cast(device_id, POINTER(c_int))))
+    print('Now at ' + str(get_position()) )
+
+if __name__ == '__main__':
+    move_to(0)
+    move_to(5000)
+    move_to(0)
+    move_to(10000)
+    move_to(0)
+
+
+
+
+
