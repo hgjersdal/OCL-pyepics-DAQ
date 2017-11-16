113c7644f489cc99d43191bb03447109929cd589
hgjersdal <hgjersdal@gmail.com>
2017-11-16 16:51:35 +0100

    Metadata should actually be written to the file

--- OCL-temperature-scan.py
+++ OCL-temperature-scan.py
@@ -4,38 +4,43 @@ import grabData, grabImage, grabSpectrum, grabPMVal, moveStage, grabTemperature
 from misc_ess.k24xx import K24xx
 
 # setup sourcemeter
-# baud = 57600
-# port = '/dev/ttyUSB2'
-# timeout = 30 # communications timeout [s]
-# nplc = 10 # number of power line cycles
-# meanValues = 15 # number of readings to internally average
-# k = K24xx(baud=baud, port=port, timeout=timeout)
-# k.currentSetup(nplc=nplc, nMean=meanValues)
+baud = 57600
+port = '/dev/ttyUSB0'
+timeout = 30 # communications timeout [s]
+nplc = 10 # number of power line cycles
+meanValues = 15 # number of readings to internally average
+k = K24xx(baud=baud, port=port, timeout=timeout)
+k.currentSetup(nplc=nplc, nMean=meanValues)
 
-with h5py.File('/var/data/ocl/2017-11-15-OCL-tempscanHV1.h5', 'w-') as h5f: #set to w- to ensure no overwriting
+with h5py.File('/var/data/ocl/2017-11-15-OCL-tempscanHV1.h5', 'w') as h5f: #set to w- to ensure no overwriting
     temperature = grabTemperature.grabTemperature()
+    temperature += 2.0 #Make sure we get the first reading immediately
     sample = 'HV1'
     while (temperature > 25):
         #Move to FC, so we are ready
         while( temperature - grabTemperature.grabTemperature() < 1 ):
             pass
-        #move to heater
+
         temperature = grabTemperature.grabTemperature()
         print('Temperature is ' + str(temperature))
         group = grabData.makePathname(str(sample) + 'temp' + str(math.floor(temperature))) #Store in group names after sample
 
         #Get Current
-        #current = k.getCurrent()
-        print('WE HAVE NO CURRENT MEASUREMENT!')
-        current = 100
+        k.setOutput(True) # turn the sourcemeter on
+        current = k.getCurrent()
+        k.setOutput(False) # turn the sourcemeter off
+        #print('WE HAVE NO CURRENT MEASUREMENT!')
+        #current = 100
         #Add meta
-        attributes = { 'Sample': sample,
-                       'Position': 5000,
-                       'CameraDistance': 1170,
-                       'BeamCurrent': current}
-        #getImages
-        subgroup = grabData.makePathname(group, 'images') #Store in group names after sample    
-        grabImage.imagesToHDF5(h5f, subgroup, 10)
+        grabData.setAttributes(h5f, group,
+                               { 'Sample': sample,
+                                 'Position': 5000,
+                                 'CameraDistance': 1170,
+                                 'BeamCurrent': current})
+        
+        #getImages NO IMAGES DUE TO CAMERA FAILURE
+        #subgroup = grabData.makePathname(group, 'images') #Store in group names after sample    
+        #grabImage.imagesToHDF5(h5f, subgroup, 10)
         #getSpectra
         subgroup = grabData.makePathname(group, 'spectum')
         grabSpectrum.spectrumToHDF5(h5f, subgroup, 10)
--- listH5.py
+++ listH5.py
@@ -6,26 +6,26 @@ def visitFunction(name, obj):
     print(name)
     for key, val in obj.attrs.iteritems():
         print( '    ' + str(key) + ': ' + str(val) )
-    if(type(obj) == h5py._hl.dataset.Dataset):
-        if(obj.len() > 100000):
-            print('I hope this is an image!')
-            raw = obj[:].reshape(964, 1292)
-            import matplotlib.pyplot as plt
-            plt.matshow(raw)
-            plt.colorbar()
-            plt.show()        
-        if(obj.len() < 100000 and obj.len()>3599):
-            print('I hope this is a spectrum!')
-            raw = obj[:]
-            import matplotlib.pyplot as plt
-            plt.plot(raw[0:3600])
-            plt.show()        
-        if(obj.len() < 3599):
-            print('Power or current scan?')
-            raw = obj[:]
-            import matplotlib.pyplot as plt
-            plt.plot(raw)
-            plt.show()        
+    # if(type(obj) == h5py._hl.dataset.Dataset):
+    #     if(obj.len() > 100000):
+    #         print('I hope this is an image!')
+    #         raw = obj[:].reshape(964, 1292)
+    #         import matplotlib.pyplot as plt
+    #         plt.matshow(raw)
+    #         plt.colorbar()
+    #         plt.show()        
+    #     if(obj.len() < 100000 and obj.len()>3599):
+    #         print('I hope this is a spectrum!')
+    #         raw = obj[:]
+    #         import matplotlib.pyplot as plt
+    #         plt.plot(raw[0:3600])
+    #         plt.show()        
+    #     if(obj.len() < 3599):
+    #         print('Power or current scan?')
+    #         raw = obj[:]
+    #         import matplotlib.pyplot as plt
+    #         plt.plot(raw)
+    #         plt.show()        
 
     
 #x = 1292, y = 964
